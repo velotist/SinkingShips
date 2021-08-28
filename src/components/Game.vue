487a10eb11031474401f0c7c80d6c63789e1c1b4
root <root@AMDDesktop.localdomain>
2021-08-28 09:51:42 +0200

    Add basic structure

---
+++ src/components/Game.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Game',
+    props: {
+        msg: String,
+    },
+})
+</script>
